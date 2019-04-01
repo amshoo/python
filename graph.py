caf7f066861153d6349e7c8ac16eba595d65c443
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-04-01 13:28:48 +0800

    add

--- graph.py
+++ graph.py
@@ -6,10 +6,10 @@
 
 import networkx as nx
 
-#G = nx.karate_club_graph()
-filepath = ('PB.txt')
-fh=open("PB.txt", 'rb')
-G = nx.read_weighted_edgelist(fh,nodetype=None, encoding='utf-8')
-fh.close()
+G = nx.karate_club_graph()
+#filepath = ('PB.txt')
+#fh=open("PB.txt", 'rb')
+#G = nx.read_weighted_edgelist(fh,nodetype=None, encoding='utf-8')
+#fh.close()
 
 H = nx.convert_node_labels_to_integers(G, first_label=0, ordering="default", label_attribute = None)
