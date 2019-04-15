9fadf1d1430d5df0270762a286f44105adb1c008
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-04-15 11:56:49 +0800

    update

--- graph.py
+++ graph.py
@@ -8,10 +8,11 @@ import networkx as nx
 
 
 
-#G = nx.karate_club_graph()
-fh=open("DataSet/NS.txt", 'rb')
-G = nx.read_weighted_edgelist(fh,nodetype=None, encoding='utf-8')
-fh.close()
+
+G = nx.karate_club_graph()
+#fh=open("DataSet/bio-celegans.txt", 'rb')
+#G = nx.read_weighted_edgelist(fh,nodetype=None, encoding='utf-8')
+#fh.close()
 
 
 H = nx.convert_node_labels_to_integers(G, first_label=0, ordering="default", label_attribute = None)
