62e8472ea75f39918713d8844a74ba2c03f8e95b
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-08-25 19:13:39 +0800

    add

--- algorithm/graph.py
+++ algorithm/graph.py
@@ -6,10 +6,10 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-H= nx.karate_club_graph()
-#fh=open("DataSet/blogs.txt", 'rb')
-#H = nx.read_weighted_edgelist(fh,nodetype=int, encoding='utf-8')
-#fh.close()
+#H= nx.karate_club_graph()
+fh=open("DataSet/dolphins.txt", 'rb')
+H = nx.read_weighted_edgelist(fh,nodetype=int, encoding='utf-8')
+fh.close()
 
 
 #H = nx.convert_node_labels_to_integers(G, first_label=0, ordering="default", label_attribute = None)
