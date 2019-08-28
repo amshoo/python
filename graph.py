52deefd8501469d8893f2f43bc966d7ab813de37
auwal amshi <25012539+amshoo@users.noreply.github.com>
2019-08-28 12:42:38 +0800

    add

--- graph.py
+++ graph.py
@@ -6,13 +6,13 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-H= nx.karate_club_graph()
-#fh=open("DataSet/email.txt", 'rb')
+G= nx.karate_club_graph()
+#fh=open("DataSet/dolphins.txt", 'rb')
 #H = nx.read_weighted_edgelist(fh,nodetype=int, encoding='utf-8')
-#fh.close()
+#SSfh.close()
 
 
-#H = nx.convert_node_labels_to_integers(G, first_label=0, ordering="default", label_attribute = None)
+H = nx.convert_node_labels_to_integers(G, first_label=1, ordering="default", label_attribute = None)
 
 def drw_graph():
     nx.draw(H, data=True)
